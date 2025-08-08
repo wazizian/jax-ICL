22fdf248146b2d749c66c1fbb680ed94638f508a
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2025-08-08 15:30:56 -0400

    Update

--- icl-task-diversity/run.sh
+++ icl-task-diversity/run.sh
@@ -6,4 +6,4 @@ git pull && \
     (trap 'kill 0' SIGINT; \
     (CUDA_VISIBLE_DEVICES=1 python run.py --config-name=plain && python analyze.py) & \
     (sleep 30 && \
-    CUDA_VISIBLE_DEVICES=2 python run.py --config-name=plain task.n_tasks=0 && python analyze.py) & wait)
+    CUDA_VISIBLE_DEVICES=2 python run.py --config-name=plain task.use_weights=true && python analyze.py) & wait)
