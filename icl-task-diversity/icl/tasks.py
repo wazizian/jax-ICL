a285dfbfbe30822348fbb729482ff7b1a44762c8
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2025-08-28 17:17:50 -0400

    Update

--- icl-task-diversity/icl/tasks.py
+++ icl-task-diversity/icl/tasks.py
@@ -173,13 +173,14 @@ def task_log_weights(
         use_weights: bool = False,
         reduce_axis: int = -1
         ) -> Array:
-    res = \
+    res = jnp.clip(
         aux_task_log_weights(
            tasks, loc, scale, clip, ref_distrib_name, ref_distrib_param, use_weights, reduce_axis
             ) \
         - aux_task_log_weights(
             tasks, loc, scale, clip, distrib_name, distrib_param, use_weights, reduce_axis
-            )
+            ),
+        0, None)
     return res
 
 
