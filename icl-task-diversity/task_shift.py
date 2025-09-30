0778c1e48c221640d4bf1bd78a089033bbb364bc
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2025-09-30 11:31:26 -0400

    Update

--- icl-task-diversity/task_shift.py
+++ icl-task-diversity/task_shift.py
@@ -404,7 +404,7 @@ def average_over_seed(run_groups: dict) -> list:
             return mean_stack(new_args)
         elif isinstance(args[0],jnp.ndarray):
             #print(f"Got jax arrays with shape {args[0].shape} and {len(args)} args")
-            shapes = [a.shape for a in new_args]
+            shapes = [a.shape for a in args]
             s = shapes[0]
             if not all([sh == s for sh in shapes]):
                 print(f"Warning: Inconsistent shapes for averaging: {shapes}, trying to recover by keeping {s}")
@@ -443,7 +443,7 @@ def average_over_seed(run_groups: dict) -> list:
                 new_args = [a for a in new_args if a.shape == s]
             ret =  std_stack(new_args)
         elif isinstance(args[0],jax.Array):
-            shapes = [a.shape for a in new_args]
+            shapes = [a.shape for a in args]
             s = shapes[0]
             if not all([sh == s for sh in shapes]):
                 print(f"Warning: Inconsistent shapes for averaging: {shapes}, trying to recover by keeping {s}")
